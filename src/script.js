5c4dee9929968e9d5686b53a0bcc2eb8e1adcef7
hungry samurai <kryjeka@mail.ru>
2023-03-31 20:48:02 +0300

    gsap integration

--- src/script.js
+++ src/script.js
@@ -5,17 +5,16 @@ const colorControlsContainer = document.querySelector(
   ".color-controls-container"
 );
 
-
 const circleContainer = document.getElementById("circle-container");
 
 const outerCircle = document.querySelector("#outer-circle");
 const innerCircle = document.querySelector("#inner-circle");
-const text = innerCircle.querySelector("#text");
+const textEl = document.querySelector("#text");
 
-const pointerContainer = document.querySelector('#pointer-container');
+const pointerContainer = document.querySelector("#pointer-container");
 const pointer = document.querySelector("#pointer");
 
-const animationControlBtn = document.querySelector('#animation-control-btn')
+const animationControlBtn = document.querySelector("#animation-control-btn");
 // Get current colorScheme from localStorage, if not found - default to 0
 if (!localStorage.getItem("relaxer-colorSchema")) {
   localStorage.setItem("relaxer-colorSchema", 0);
@@ -23,10 +22,9 @@ if (!localStorage.getItem("relaxer-colorSchema")) {
 
 let currentColorSchema = +localStorage.getItem("relaxer-colorSchema");
 
-
 let mainTimeLine, granimBG;
 
-// Color themes
+// Color themes for BG
 const gradients = [
   [
     ["#231942", "#E0B1CB"],
@@ -50,23 +48,6 @@ const gradients = [
   ],
 ];
 
-// Animation
-// function breathAnimation() {
-//   text.innerText = "Вдох";
-//   container.className = "container grow";
-
-//   setTimeout(() => {
-//     text.innerText = "Держим";
-
-//     setTimeout(() => {
-//       text.innerText = "Выдох";
-//       container.className = "container shrink";
-//     }, holdTime);
-//   }, breathInTime);
-// }
-
-
-
 // Switch Color Schema
 function setColorSchema(schema) {
   // Clean container
@@ -75,12 +56,10 @@ function setColorSchema(schema) {
   // Update Container
   updateColorSwitcher(schema);
 
-  // Update styles in DOM
-  innerCircle.style.animationName = `change-bg-${schema}`;
   outerCircle.style.background = `var(--gradient${schema})`;
-  pointer.style.animationName = `change-bg-${schema}`;
 
   // New gradient background
+  if (granimBG) granimBG.destroy();
   granimBG = new Granim({
     element: "#granim-canvas",
     direction: "radial",
@@ -92,11 +71,11 @@ function setColorSchema(schema) {
       },
     },
   });
+  granimBG.pause();
 
+  currentColorSchema = schema;
 
-  currentColorSchema = schema
   initBreathAnimation();
-
 }
 
 // Color switcher container - populate
@@ -141,9 +120,6 @@ window.onblur = function () {
   audio.pause();
 };
 
-
-
-
 // const breathInTime = 4000;
 // const holdTime = 7000;
 // const breathOutTime = 8000;
@@ -159,8 +135,7 @@ window.onblur = function () {
 //   { duration: 19 }
 // ]
 
-
-animationControlBtn.addEventListener('click', () => {
+animationControlBtn.addEventListener("click", () => {
   if (mainTimeLine) {
     if (mainTimeLine._ts) {
       mainTimeLine.pause();
@@ -169,151 +144,225 @@ animationControlBtn.addEventListener('click', () => {
       // mainTimeLine.restart();
     } else {
       mainTimeLine.play();
-      granimBG.play()
+      granimBG.play();
     }
   }
-})
+});
 
 function initBreathAnimation() {
   if (mainTimeLine) {
+    // Reset all animations
     mainTimeLine.kill();
+    updateText("");
+    gsap.to(circleContainer, {
+      scale: 1,
+      duration: 0.2,
+      ease: "Power4.in",
+    });
+
+    gsap.to(outerCircle, {
+      boxShadow: "0rem 0rem 1px rgba(0, 0, 0, 0.1)",
+      duration: 0.2,
+      ease: "Power4.in",
+    });
+
+    gsap.to(innerCircle, {
+      scale: 0.75,
+      duration: 0.2,
+      ease: "Power4.in",
+    });
 
     gsap.to(pointerContainer, {
       rotate: 0,
-      duration: 0.1,
-      ease: "Power4.in"
-    })
+      duration: 0.2,
+      ease: "Power4.in",
+    });
   }
 
-  console.log(gradients[currentColorSchema]);
-
-  mainTimeLine = gsap.timeline(
-    {
-      defaults: { repeat: -1, duration: 19 }
-    });
-
-
-  mainTimeLine.to(circleContainer, {
-    keyframes: {
-      '0%': {
-        scale: 1, onComplete: () => {
-          text.innerText = 'Вдох';
-        },
-      },
-      '21%': {
-        scale: 1.2,
-        onComplete: () => {
-          text.innerText = 'Держим';
-        }
-      },
-      '47%': {
-        scale: 1.2,
-        onComplete: () => {
-          text.innerText = 'Выдох';
-        }
-      },
-      '100%': {
-        scale: 1,
-        onComplete: () => {
-          text.innerText = 'Вдох';
-        }
-      }
-    }
+  mainTimeLine = gsap.timeline({
+    repeat: -1,
+    paused: true,
+    defaults: { duration: 19 },
   });
 
-
-  mainTimeLine.to(outerCircle, {
+  // Container shrink/grow
+  mainTimeLine.to(circleContainer, {
     keyframes: {
-      '0%': {
-        boxShadow: '0rem 0rem 1px rgba(0, 0, 0, 0.1)'
+      "0%": {
+        scale: 1,
       },
-      '21%': {
-        boxShadow: '0.1rem 0.1rem 86px 4px rgba(0, 0, 0, 0.5)'
+      "21%": {
+        scale: 1.3,
       },
-      '47%': {
-        boxShadow: '0.1rem 0.1rem 86px 4px rgba(0, 0, 0, 0.5)'
+      "47%": {
+        scale: 1.3,
       },
-      '100%': {
-        boxShadow: '0rem 0rem 1px rgba(0, 0, 0, 0.7)'
+      "100%": {
+        scale: 1,
       },
-      ease: "linear",
-    }
-  }, '<')
-
-  mainTimeLine.to(innerCircle, {
-    keyframes: {
-      '0%': {
-        backgroundColor: gradients[currentColorSchema][0][0],
-        ease: "linear"
-      },
-      '21%': {
-        backgroundColor: gradients[currentColorSchema][0][1],
-        ease: "Power1.easeIn"
-      },
-      '47%': {
-        backgroundColor: gradients[currentColorSchema][0][1],
-        ease: "linear"
-      },
-      '100%': {
-        backgroundColor: gradients[currentColorSchema][0][0],
-        ease: "Power1.easeIn"
-      }
-    }
-  }, '<')
-
-  mainTimeLine.to(pointerContainer, {
-    keyframes: {
-      '0%': {
-        rotate: 0,
-        ease: "linear"
-      },
-      '21%': {
-        rotate: 76,
-        ease: "Power1.easeIn"
-      },
-      '47%': {
-        rotate: 120,
-        ease: "linear"
-
-      },
-      '100%': {
-        rotate: 360,
-        ease: "Power1.easeIn"
-      }
     },
-  }, '<0.2')
+  });
 
-  gsap.set(pointer, { backgroundColor: gradients[currentColorSchema][0][1] })
-
-  mainTimeLine.to(pointer, {
-    keyframes: {
-      '0%': {
-        backgroundColor: gradients[currentColorSchema][0][1]
+  mainTimeLine.to(
+    textEl,
+    {
+      keyframes: {
+        "0%": {
+          onComplete: () => {
+            updateText("Вдох");
+          },
+        },
+        "21%": {
+          onComplete: () => {
+            updateText("Держим");
+          },
+        },
+        "47%": {
+          onComplete: () => {
+            updateText("Выдох");
+          },
+        },
+        // "100%": {
+        //   onStart: () => {
+        //     updateText("Вдох");
+        //   },
+        // },
       },
-      '21%': {
-        backgroundColor: gradients[currentColorSchema][0][0]
-      },
-      '47%': {
-        backgroundColor: gradients[currentColorSchema][0][0]
-
-      },
-      '100%': {
-        backgroundColor: gradients[currentColorSchema][0][1]
-      }
     },
-  }, '<')
+    "<"
+  );
 
+  // Outer Circle shadow dynamic
+  mainTimeLine.to(
+    outerCircle,
+    {
+      keyframes: {
+        "0%": {
+          boxShadow: "0rem 0rem 1px rgba(0, 0, 0, 0.1)",
+        },
+        "21%": {
+          boxShadow: "0.1rem 0.1rem 86px 4px rgba(0, 0, 0, 0.5)",
+        },
+        "47%": {
+          boxShadow: "0.1rem 0.1rem 86px 4px rgba(0, 0, 0, 0.5)",
+        },
+        "100%": {
+          boxShadow: "0rem 0rem 1px rgba(0, 0, 0, 0.7)",
+        },
+        ease: "linear",
+      },
+    },
+    "<"
+  );
+
+  // Inner circle BG
+  mainTimeLine.to(
+    innerCircle,
+    {
+      keyframes: {
+        "0%": {
+          backgroundColor: gradients[currentColorSchema][0][0],
+          ease: "linear",
+        },
+        "21%": {
+          backgroundColor: gradients[currentColorSchema][0][1],
+          ease: "Power1.easeIn",
+        },
+        "47%": {
+          backgroundColor: gradients[currentColorSchema][0][1],
+          ease: "linear",
+        },
+        "100%": {
+          backgroundColor: gradients[currentColorSchema][0][0],
+          ease: "Power1.easeIn",
+        },
+      },
+    },
+    "<"
+  );
+
+  gsap.set(innerCircle, {
+    backgroundColor: gradients[currentColorSchema][0][0],
+    scale: 0.75,
+  });
+
+  mainTimeLine.to(
+    innerCircle,
+    {
+      keyframes: {
+        "0%": {
+          scale: 0.75,
+          ease: "linear",
+        },
+        "21%": {
+          scale: 0.9,
+          ease: "Power1.easeIn",
+        },
+        "47%": {
+          scale: 0.95,
+          ease: "linear",
+        },
+        "100%": {
+          scale: 0.75,
+          ease: "Power1.easeIn",
+        },
+      },
+    },
+    "<"
+  );
+
+  mainTimeLine.to(
+    pointerContainer,
+    {
+      keyframes: {
+        "0%": {
+          rotate: 0,
+          ease: "linear",
+        },
+        "21%": {
+          rotate: 76,
+          ease: "Power1.easeIn",
+        },
+        "47%": {
+          rotate: 120,
+          ease: "linear",
+        },
+        "100%": {
+          rotate: 360,
+          ease: "Power1.easeIn",
+        },
+      },
+    },
+    "<0.2"
+  );
+
+  gsap.set(pointer, { backgroundColor: gradients[currentColorSchema][0][1] });
+
+  mainTimeLine.to(
+    pointer,
+    {
+      keyframes: {
+        "0%": {
+          backgroundColor: gradients[currentColorSchema][0][1],
+        },
+        "21%": {
+          backgroundColor: gradients[currentColorSchema][0][0],
+        },
+        "47%": {
+          backgroundColor: gradients[currentColorSchema][0][0],
+        },
+        "100%": {
+          backgroundColor: gradients[currentColorSchema][0][1],
+        },
+      },
+    },
+    "<"
+  );
 }
 
 function updateText(string) {
-  console.log('call', string);
-  text.innerText = string;
+  textEl.textContent = string;
 }
-
 
 // Init color schema
 setColorSchema(currentColorSchema);
-
-// Init animations
-// initBreathAnimation();
-// setInterval(breathAnimation, 19000);

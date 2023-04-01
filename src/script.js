d22624f608814c7b301105357372743105b80e49
hungry samurai <kryjeka@mail.ru>
2023-04-01 12:02:18 +0300

    refactor done

--- src/script.js
+++ src/script.js
@@ -1,12 +1,7 @@
-// DOM
-const soundButton = document.querySelector(".soundbutton"),
-  audio = document.querySelector(".audio");
-const colorControlsContainer = document.querySelector(
-  ".color-controls-container"
-);
+// DOM elements
+const animationControlBtn = document.querySelector("#animation-control-btn");
 
 const circleContainer = document.getElementById("circle-container");
-
 const outerCircle = document.querySelector("#outer-circle");
 const innerCircle = document.querySelector("#inner-circle");
 const textEl = document.querySelector("#text");
@@ -14,17 +9,35 @@ const textEl = document.querySelector("#text");
 const pointerContainer = document.querySelector("#pointer-container");
 const pointer = document.querySelector("#pointer");
 
-const animationControlBtn = document.querySelector("#animation-control-btn");
+const colorControlsContainer = document.querySelector(
+  ".color-controls-container"
+);
+const modeBtns = document.querySelectorAll(".mode-btn");
+const soundButton = document.querySelector(".soundbutton"),
+  audio = document.querySelector(".audio");
+const animationStateicon = animationControlBtn.querySelector("i");
+
+let currentColorSchema, currentMode, currentDuration, mainTimeLine, granimBG;
+
 // Get current colorScheme from localStorage, if not found - default to 0
 if (!localStorage.getItem("relaxer-colorSchema")) {
   localStorage.setItem("relaxer-colorSchema", 0);
+  currentColorSchema = 0;
+} else {
+  currentColorSchema = localStorage.getItem("relaxer-colorSchema");
 }
 
-let currentColorSchema = +localStorage.getItem("relaxer-colorSchema");
+// Get current mode from localStorage, if noot found - default to 4-7-8
+if (!localStorage.getItem("relaxer-mode")) {
+  localStorage.setItem("relaxer-mode", "4-7-8");
+  currentMode = "4-7-8";
+} else {
+  currentMode = localStorage.getItem("relaxer-mode");
+}
 
-let mainTimeLine, granimBG;
+// Data
 
-// Color themes for BG
+// Gradients sets
 const gradients = [
   [
     ["#231942", "#E0B1CB"],
@@ -48,7 +61,270 @@ const gradients = [
   ],
 ];
 
-// Switch Color Schema
+//Keyframes sets
+const getKeyframes = (mode) => {
+  if (mode === "4-7-8") {
+    return {
+      circleContainer: {
+        keyframes: {
+          "0%": {
+            scale: 1,
+          },
+          "21%": {
+            scale: 1.3,
+          },
+          "47%": {
+            scale: 1.3,
+          },
+          "100%": {
+            scale: 1,
+          },
+        },
+      },
+      textEl: {
+        keyframes: {
+          "0%": {
+            onComplete: () => {
+              updateText("Вдох");
+            },
+          },
+          "21%": {
+            onComplete: () => {
+              updateText("Держим");
+            },
+          },
+          "47%": {
+            onComplete: () => {
+              updateText("Выдох");
+            },
+          },
+        },
+      },
+      outerCircle: {
+        keyframes: {
+          "0%": {
+            boxShadow: "0rem 0rem 1px rgba(0, 0, 0, 0.1)",
+          },
+          "21%": {
+            boxShadow: "0.1rem 0.1rem 86px 4px rgba(0, 0, 0, 0.5)",
+          },
+          "47%": {
+            boxShadow: "0.1rem 0.1rem 86px 4px rgba(0, 0, 0, 0.5)",
+          },
+          "100%": {
+            boxShadow: "0rem 0rem 1px rgba(0, 0, 0, 0.7)",
+          },
+          ease: "linear",
+        },
+      },
+
+      innerCircle: {
+        keyframes: {
+          "0%": {
+            scale: 0.75,
+            backgroundColor: gradients[currentColorSchema][0][0],
+            ease: "linear",
+          },
+          "21%": {
+            scale: 0.9,
+            backgroundColor: gradients[currentColorSchema][0][1],
+            ease: "Power1.easeIn",
+          },
+          "47%": {
+            scale: 0.95,
+            backgroundColor: gradients[currentColorSchema][0][1],
+            ease: "linear",
+          },
+          "100%": {
+            scale: 0.75,
+            backgroundColor: gradients[currentColorSchema][0][0],
+            ease: "Power1.easeIn",
+          },
+        },
+      },
+
+      pointerContainer: {
+        keyframes: {
+          "0%": {
+            rotate: 0,
+            ease: "linear",
+          },
+          "21%": {
+            rotate: 76,
+            ease: "Power1.easeIn",
+          },
+          "47%": {
+            rotate: 120,
+            ease: "linear",
+          },
+          "100%": {
+            rotate: 360,
+            ease: "Power1.easeIn",
+          },
+        },
+      },
+
+      pointer: {
+        keyframes: {
+          "0%": {
+            backgroundColor: gradients[currentColorSchema][0][1],
+          },
+          "21%": {
+            backgroundColor: gradients[currentColorSchema][0][0],
+          },
+          "47%": {
+            backgroundColor: gradients[currentColorSchema][0][0],
+          },
+          "100%": {
+            backgroundColor: gradients[currentColorSchema][0][1],
+          },
+        },
+      },
+    };
+  } else if (mode === "4-4-4-4") {
+    return {
+      circleContainer: {
+        keyframes: {
+          "0%": {
+            scale: 1,
+          },
+          "25%": {
+            scale: 1.3,
+          },
+          "50%": {
+            scale: 1.3,
+          },
+          "75%": {
+            scale: 1,
+          },
+          "100%": {
+            scale: 1,
+          },
+        },
+      },
+      textEl: {
+        keyframes: {
+          "0%": {
+            onComplete: () => {
+              updateText("Вдох");
+            },
+          },
+          "25%": {
+            onComplete: () => {
+              updateText("Держим");
+            },
+          },
+          "50%": {
+            onComplete: () => {
+              updateText("Выдох");
+            },
+          },
+          "75%": {
+            onComplete: () => {
+              updateText("Держим");
+            },
+          },
+        },
+      },
+      outerCircle: {
+        keyframes: {
+          "0%": {
+            boxShadow: "0rem 0rem 1px rgba(0, 0, 0, 0.1)",
+          },
+          "25%": {
+            boxShadow: "0.1rem 0.1rem 86px 4px rgba(0, 0, 0, 0.5)",
+          },
+          "50%": {
+            boxShadow: "0.1rem 0.1rem 86px 4px rgba(0, 0, 0, 0.5)",
+          },
+          "75%": {
+            boxShadow: "0rem 0rem 1px rgba(0, 0, 0, 0.1)",
+          },
+          "100%": {
+            boxShadow: "0rem 0rem 1px rgba(0, 0, 0, 0.1)",
+          },
+          ease: "linear",
+        },
+      },
+
+      innerCircle: {
+        keyframes: {
+          "0%": {
+            scale: 0.75,
+            backgroundColor: gradients[currentColorSchema][0][0],
+            ease: "linear",
+          },
+          "25%": {
+            scale: 0.9,
+            backgroundColor: gradients[currentColorSchema][0][1],
+            ease: "Power1.easeIn",
+          },
+          "50%": {
+            scale: 0.95,
+            backgroundColor: gradients[currentColorSchema][0][1],
+            ease: "linear",
+          },
+          "75%": {
+            scale: 0.75,
+            backgroundColor: gradients[currentColorSchema][0][0],
+            ease: "Power1.easeIn",
+          },
+          "100%": {
+            scale: 0.75,
+            backgroundColor: gradients[currentColorSchema][0][0],
+            ease: "Power1.easeIn",
+          },
+        },
+      },
+
+      pointerContainer: {
+        keyframes: {
+          "0%": {
+            rotate: 0,
+            ease: "linear",
+          },
+          "25%": {
+            rotate: 90,
+            ease: "Power1.easeIn",
+          },
+          "50%": {
+            rotate: 180,
+            ease: "linear",
+          },
+          "75%": {
+            rotate: 270,
+            ease: "linear",
+          },
+          "100%": {
+            rotate: 360,
+            ease: "Power1.easeIn",
+          },
+        },
+      },
+
+      pointer: {
+        keyframes: {
+          "0%": {
+            backgroundColor: gradients[currentColorSchema][0][1],
+          },
+          "25%": {
+            backgroundColor: gradients[currentColorSchema][0][0],
+          },
+          "50%": {
+            backgroundColor: gradients[currentColorSchema][0][0],
+          },
+          "75%": {
+            backgroundColor: gradients[currentColorSchema][0][1],
+          },
+          "100%": {
+            backgroundColor: gradients[currentColorSchema][0][1],
+          },
+        },
+      },
+    };
+  }
+};
+
 function setColorSchema(schema) {
   // Clean container
   colorControlsContainer.innerHTML = "";
@@ -60,6 +336,7 @@ function setColorSchema(schema) {
 
   // New gradient background
   if (granimBG) granimBG.destroy();
+
   granimBG = new Granim({
     element: "#granim-canvas",
     direction: "radial",
@@ -67,15 +344,14 @@ function setColorSchema(schema) {
     states: {
       "default-state": {
         gradients: [gradients[schema][0], gradients[schema][1]],
-        transitionSpeed: 19000 / 2,
+        transitionSpeed: (currentDuration * 1000) / 2,
       },
     },
   });
+
   granimBG.pause();
 
   currentColorSchema = schema;
-
-  initBreathAnimation();
 }
 
 // Color switcher container - populate
@@ -90,7 +366,9 @@ function updateColorSwitcher(schema) {
       element.addEventListener("click", (e) => {
         const newSchema = +e.target.dataset.color;
         localStorage.setItem("relaxer-colorSchema", newSchema);
-        setColorSchema(newSchema);
+        currentColorSchema = newSchema;
+
+        init();
       });
     }
 
@@ -101,59 +379,23 @@ function updateColorSwitcher(schema) {
   });
 }
 
-// Toggle color changer container
-colorControlsContainer.addEventListener("click", (e) => {
-  colorControlsContainer.classList.toggle("folded");
-});
+function setAnimation() {
+  // Default some elements
+  gsap.set(innerCircle, {
+    backgroundColor: gradients[currentColorSchema][0][0],
+    scale: 0.75,
+  });
+  gsap.set(pointer, { backgroundColor: gradients[currentColorSchema][0][1] });
 
-// Audio
-soundButton.addEventListener("click", (e) => {
-  soundButton.classList.toggle("paused");
-  audio.paused ? audio.play() : audio.pause();
-});
+  getAnimation(getKeyframes(currentMode));
+}
 
-window.onfocus = function () {
-  soundButton.classList.contains("paused") ? audio.pause() : audio.play();
-};
-
-window.onblur = function () {
-  audio.pause();
-};
-
-// const breathInTime = 4000;
-// const holdTime = 7000;
-// const breathOutTime = 8000;
-// const totalTime = 19000;
-
-// const data478 = [
-//   {
-//     '0s': { scale: 1 },
-//     '4s': { scale: 1.2 },
-//     '11s': { scale: 1.2 },
-//     '19s': { scale: 1 }
-//   },
-//   { duration: 19 }
-// ]
-
-animationControlBtn.addEventListener("click", () => {
-  if (mainTimeLine) {
-    if (mainTimeLine._ts) {
-      mainTimeLine.pause();
-      granimBG.pause();
-
-      // mainTimeLine.restart();
-    } else {
-      mainTimeLine.play();
-      granimBG.play();
-    }
-  }
-});
-
-function initBreathAnimation() {
+function getAnimation(options) {
   if (mainTimeLine) {
     // Reset all animations
     mainTimeLine.kill();
     updateText("");
+
     gsap.to(circleContainer, {
       scale: 1,
       duration: 0.2,
@@ -179,190 +421,96 @@ function initBreathAnimation() {
     });
   }
 
+  // Init timeline
   mainTimeLine = gsap.timeline({
     repeat: -1,
     paused: true,
-    defaults: { duration: 19 },
+    defaults: { duration: currentDuration },
   });
 
-  // Container shrink/grow
-  mainTimeLine.to(circleContainer, {
-    keyframes: {
-      "0%": {
-        scale: 1,
-      },
-      "21%": {
-        scale: 1.3,
-      },
-      "47%": {
-        scale: 1.3,
-      },
-      "100%": {
-        scale: 1,
-      },
-    },
-  });
-
-  mainTimeLine.to(
-    textEl,
-    {
-      keyframes: {
-        "0%": {
-          onComplete: () => {
-            updateText("Вдох");
-          },
-        },
-        "21%": {
-          onComplete: () => {
-            updateText("Держим");
-          },
-        },
-        "47%": {
-          onComplete: () => {
-            updateText("Выдох");
-          },
-        },
-        // "100%": {
-        //   onStart: () => {
-        //     updateText("Вдох");
-        //   },
-        // },
-      },
-    },
-    "<"
-  );
-
-  // Outer Circle shadow dynamic
-  mainTimeLine.to(
-    outerCircle,
-    {
-      keyframes: {
-        "0%": {
-          boxShadow: "0rem 0rem 1px rgba(0, 0, 0, 0.1)",
-        },
-        "21%": {
-          boxShadow: "0.1rem 0.1rem 86px 4px rgba(0, 0, 0, 0.5)",
-        },
-        "47%": {
-          boxShadow: "0.1rem 0.1rem 86px 4px rgba(0, 0, 0, 0.5)",
-        },
-        "100%": {
-          boxShadow: "0rem 0rem 1px rgba(0, 0, 0, 0.7)",
-        },
-        ease: "linear",
-      },
-    },
-    "<"
-  );
-
-  // Inner circle BG
-  mainTimeLine.to(
-    innerCircle,
-    {
-      keyframes: {
-        "0%": {
-          backgroundColor: gradients[currentColorSchema][0][0],
-          ease: "linear",
-        },
-        "21%": {
-          backgroundColor: gradients[currentColorSchema][0][1],
-          ease: "Power1.easeIn",
-        },
-        "47%": {
-          backgroundColor: gradients[currentColorSchema][0][1],
-          ease: "linear",
-        },
-        "100%": {
-          backgroundColor: gradients[currentColorSchema][0][0],
-          ease: "Power1.easeIn",
-        },
-      },
-    },
-    "<"
-  );
-
-  gsap.set(innerCircle, {
-    backgroundColor: gradients[currentColorSchema][0][0],
-    scale: 0.75,
-  });
-
-  mainTimeLine.to(
-    innerCircle,
-    {
-      keyframes: {
-        "0%": {
-          scale: 0.75,
-          ease: "linear",
-        },
-        "21%": {
-          scale: 0.9,
-          ease: "Power1.easeIn",
-        },
-        "47%": {
-          scale: 0.95,
-          ease: "linear",
-        },
-        "100%": {
-          scale: 0.75,
-          ease: "Power1.easeIn",
-        },
-      },
-    },
-    "<"
-  );
-
-  mainTimeLine.to(
-    pointerContainer,
-    {
-      keyframes: {
-        "0%": {
-          rotate: 0,
-          ease: "linear",
-        },
-        "21%": {
-          rotate: 76,
-          ease: "Power1.easeIn",
-        },
-        "47%": {
-          rotate: 120,
-          ease: "linear",
-        },
-        "100%": {
-          rotate: 360,
-          ease: "Power1.easeIn",
-        },
-      },
-    },
-    "<0.2"
-  );
-
-  gsap.set(pointer, { backgroundColor: gradients[currentColorSchema][0][1] });
-
-  mainTimeLine.to(
-    pointer,
-    {
-      keyframes: {
-        "0%": {
-          backgroundColor: gradients[currentColorSchema][0][1],
-        },
-        "21%": {
-          backgroundColor: gradients[currentColorSchema][0][0],
-        },
-        "47%": {
-          backgroundColor: gradients[currentColorSchema][0][0],
-        },
-        "100%": {
-          backgroundColor: gradients[currentColorSchema][0][1],
-        },
-      },
-    },
-    "<"
-  );
+  mainTimeLine.to(circleContainer, options.circleContainer);
+  mainTimeLine.to(textEl, options.textEl, "<");
+  mainTimeLine.to(outerCircle, options.outerCircle, "<");
+  mainTimeLine.to(innerCircle, options.innerCircle, "<");
+  mainTimeLine.to(pointerContainer, options.pointerContainer, "<0.2");
+  mainTimeLine.to(pointer, options.pointer, "<");
 }
 
 function updateText(string) {
   textEl.textContent = string;
 }
 
-// Init color schema
-setColorSchema(currentColorSchema);
+function getTotalDuration(string) {
+  return string
+    .split("-")
+    .map((n) => parseInt(n))
+    .reduce((a, c) => a + c);
+}
+
+// Mode buttons
+modeBtns.forEach((btn) => {
+  btn.addEventListener("click", (e) => {
+    modeBtns.forEach((btn) => btn.classList.remove("active"));
+    e.target.classList.add("active");
+
+    const mode = e.target.dataset.mode;
+
+    currentDuration = getTotalDuration(mode);
+    currentMode = mode;
+    localStorage.setItem("relaxer-mode", mode);
+
+    init();
+  });
+});
+
+// Toggle color changer container
+colorControlsContainer.addEventListener("click", (e) => {
+  colorControlsContainer.classList.toggle("folded");
+});
+
+// Play/pause button
+
+animationControlBtn.addEventListener("click", () => {
+  if (animationStateicon.classList.contains("fa-play")) {
+    animationStateicon.classList.remove("fa-play");
+    animationStateicon.classList.add("fa-pause");
+  } else {
+    animationStateicon.classList.remove("fa-pause");
+    animationStateicon.classList.add("fa-play");
+  }
+  if (mainTimeLine) {
+    if (mainTimeLine._ts) {
+      mainTimeLine.pause();
+      granimBG.pause();
+    } else {
+      mainTimeLine.play();
+      granimBG.play();
+    }
+  }
+});
+
+// Audio button
+soundButton.addEventListener("click", (e) => {
+  soundButton.classList.toggle("paused");
+  audio.paused ? audio.play() : audio.pause();
+});
+
+window.onfocus = function () {
+  soundButton.classList.contains("paused") ? audio.pause() : audio.play();
+};
+
+window.onblur = function () {
+  audio.pause();
+};
+
+// Init/ re-init animation & color schema
+function init() {
+  animationStateicon.className = "fa-solid fa-play";
+  currentDuration = getTotalDuration(currentMode);
+
+  setColorSchema(+currentColorSchema);
+
+  setAnimation();
+}
+
+init();
